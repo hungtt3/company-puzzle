f0b66e30c30fc3df750a878a5d22d4eeb99b878d
Warm & Cold <151762478+hungtt3@users.noreply.github.com>
2023-11-28 15:20:05 +0000

    Update 3 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect,ReactNode, useRef, useState } from "react";
 import logo1 from "./img/NCB1.png";
 import logo2 from "./img/Teller.png";
 import Crossword, {
@@ -31,74 +31,82 @@ import "./App.css";
 //     },
 //   },
 // };
-// BƯỚC 1: DỮ LIỆU ĐƯA VÀO ĐÂY
 const data = {
   //HÀNG NGANG
   across: {
     1: {
       clue: "one plus one",
-      answer: "XÔNGĐẤT",
+      answer: "TACPHONG",
       row: 0,
       col: 5,
     },
     2: {
-      clue: "capital of France",
-      answer: "GIAOTHỪA",
+      clue: "one plus one",
+      answer: "ANPHAT",
       row: 1,
-      col: 7,
+      col: 4,
     },
     3: {
-      clue: "popular programming language",
-      answer: "ĐÁNHĐU",
+      clue: "one plus one",
+      answer: "LAMCHUCONGNGHE",
       row: 2,
       col: 2,
     },
     4: {
-      clue: "popular programming language",
-      answer: "CÂYNÊU",
+      clue: "one plus one",
+      answer: "QUANLY",
       row: 3,
-      col: 5,
-    },
-    5: {
-      clue: "popular programming language",
-      answer: "TẤTNIÊN",
-      row: 4,
       col: 2,
     },
+    5: {
+      clue: "one plus one",
+      answer: "BAOVETAISAN",
+      row: 4,
+      col: 3,
+    },
     6: {
-      clue: "popular programming language",
-      answer: "NGÃBASÔNG",
+      clue: "one plus one",
+      answer: "CONSO",
       row: 5,
-      col: 0,
+      col: 5,
     },
     7: {
-      clue: "popular programming language",
-      answer: "CÂUĐỐI",
+      clue: "one plus one",
+      answer: "HATXAM",
       row: 6,
-      col: 4,
+      col: 2,
     },
     8: {
-      clue: "popular programming language",
-      answer: "HÁTXOAN",
+      clue: "one plus one",
+      answer: "KIEMSOAT",
       row: 7,
       col: 6,
     },
     9: {
-      clue: "popular programming language",
-      answer: "BÁNHCHƯNG",
+      clue: "one plus one",
+      answer: "BANHCHUNG",
       row: 8,
       col: 5,
     },
+    10: {
+      clue: "one plus one",
+      answer: "CHUYENKHOAN",
+      row: 9,
+      col: 0,
+    },
+    
     // Add more across clues as needed
   },
   down: {
-    // HÀNG DỌC
-    10: {
-      clue: "popular programming language",
-      answer: "NGUYÊNĐÁN",
+    11: {
+      clue: "one plus one",
+      answer: "CHUYENMINH",
       row: 0,
       col: 7,
     },
+
+    // HÀNG DỌC
+
     // Add more down clues as needed
   },
 };
@@ -112,16 +120,21 @@ const Header = styled.h1`
   margin-bottom: 1em;
 `;
 
-const Commands = styled.div``;
+const Commands = styled.div`
+  padding-top: 5rem;
+`;
 
 const Command = styled.button`
   margin-right: 1em;
+  display: flex;
+  padding: 1em;
 `;
 
 const CrosswordMessageBlock = styled.div`
   margin: 2em 0 4em;
-
-  gap: 2em;
+  display: flex;
+  gap: 30em;
+  padding-top: 10rem;
 `;
 
 const CrosswordWrapper = styled.div`
@@ -165,12 +178,10 @@ const CrosswordProviderWrapper = styled(CrosswordWrapper)`
   display: flex;
   gap: 1em;
 
-  z-index: -1;
-
   .grid {
     width: 60em;
 
-    padding-top: 6rem;
+    padding-top: 5rem;
   }
 `;
 
@@ -225,7 +236,7 @@ function App() {
     (direction, number, answer) => {
       addMessage(`onCorrect: "${direction}", "${number}", "${answer}"`);
     },
-    [addMessage],
+    [addMessage]
   );
 
   // onLoadedCorrect is called with an array of the already-correct answers,
@@ -239,12 +250,12 @@ function App() {
         `onLoadedCorrect:\n${answers
           .map(
             ([direction, number, answer]) =>
-              `    - "${direction}", "${number}", "${answer}"`,
+              `    - "${direction}", "${number}", "${answer}"`
           )
-          .join("\n")}`,
+          .join("\n")}`
       );
     },
-    [addMessage],
+    [addMessage]
   );
 
   // onCrosswordCorrect is called with a truthy/falsy value.
@@ -254,7 +265,7 @@ function App() {
     (isCorrect) => {
       addMessage(`onCrosswordCorrect: ${JSON.stringify(isCorrect)}`);
     },
-    [addMessage],
+    [addMessage]
   );
 
   // Update your existing revealRowAnswer function
@@ -264,121 +275,173 @@ function App() {
     (row, col, char) => {
       addMessage(`onCellChange: "${row}", "${col}", "${char}"`);
     },
-    [addMessage],
+    [addMessage]
   );
 
   // all the same functionality, but for the decomposed CrosswordProvider
   const crosswordProvider = useRef<CrosswordProviderImperative>(null);
-
-  // BƯỚC 2: ĐIỀN CÂU LỆNH RA ĐÁP ÁN TỪNG DÒNG (SỐ CỦA HÀNG DỌC,SỐ CỦA HÀNG NGANG, TỪ ĐIỀN VÀO)
   const fillAnswer1 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(0, 5, "X");
-    crosswordProvider.current?.setGuess(0, 6, "Ô");
-    crosswordProvider.current?.setGuess(0, 7, "N");
-    crosswordProvider.current?.setGuess(0, 8, "g");
-    crosswordProvider.current?.setGuess(0, 9, "Đ");
-    crosswordProvider.current?.setGuess(0, 10, "Ấ");
-    crosswordProvider.current?.setGuess(0, 11, "T");
+    crosswordProvider.current?.setGuess(0, 5, "T");
+    crosswordProvider.current?.setGuess(0, 6, "A");
+    crosswordProvider.current?.setGuess(0, 7, "C");
+    crosswordProvider.current?.setGuess(0, 8, "P");
+    crosswordProvider.current?.setGuess(0, 9, "H");
+    crosswordProvider.current?.setGuess(0, 10, "O");
+    crosswordProvider.current?.setGuess(0, 11, "N");
+    crosswordProvider.current?.setGuess(0, 12, "G");
+   
   }, []);
 
   const fillAnswer2 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(1, 7, "g");
-    crosswordProvider.current?.setGuess(1, 8, "i");
-    crosswordProvider.current?.setGuess(1, 9, "a");
-    crosswordProvider.current?.setGuess(1, 10, "o");
-    crosswordProvider.current?.setGuess(1, 11, "t");
-    crosswordProvider.current?.setGuess(1, 12, "h");
-    crosswordProvider.current?.setGuess(1, 13, "ừ");
-    crosswordProvider.current?.setGuess(1, 14, "a");
+    crosswordProvider.current?.setGuess(1, 4, "A");
+    crosswordProvider.current?.setGuess(1, 5, "N");
+    crosswordProvider.current?.setGuess(1, 6, "P");
+    crosswordProvider.current?.setGuess(1, 7, "H");
+    crosswordProvider.current?.setGuess(1, 8, "A");
+    crosswordProvider.current?.setGuess(1, 9, "T");
   }, []);
   const fillAnswer3 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(2, 2, "đ");
-    crosswordProvider.current?.setGuess(2, 3, "á");
-    crosswordProvider.current?.setGuess(2, 4, "n");
-    crosswordProvider.current?.setGuess(2, 5, "h");
-    crosswordProvider.current?.setGuess(2, 6, "đ");
-    crosswordProvider.current?.setGuess(2, 7, "u");
+    crosswordProvider.current?.setGuess(2, 2, "L");
+    crosswordProvider.current?.setGuess(2, 3, "A");
+    crosswordProvider.current?.setGuess(2, 4, "M");
+    crosswordProvider.current?.setGuess(2, 5, "C");
+    crosswordProvider.current?.setGuess(2, 6, "H");
+    crosswordProvider.current?.setGuess(2, 7, "U");
+    crosswordProvider.current?.setGuess(2, 8, "C");
+    crosswordProvider.current?.setGuess(2, 9, "O");
+    crosswordProvider.current?.setGuess(2, 10, "N");
+    crosswordProvider.current?.setGuess(2, 11, "G");
+    crosswordProvider.current?.setGuess(2, 12, "N");
+    crosswordProvider.current?.setGuess(2, 13, "G");
+    crosswordProvider.current?.setGuess(2, 14, "H");
+    crosswordProvider.current?.setGuess(2, 15, "E");
+   
+    
   }, []);
   const fillAnswer4 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(3, 5, "c");
-    crosswordProvider.current?.setGuess(3, 6, "â");
-    crosswordProvider.current?.setGuess(3, 7, "y");
-    crosswordProvider.current?.setGuess(3, 8, "n");
-    crosswordProvider.current?.setGuess(3, 9, "ê");
-    crosswordProvider.current?.setGuess(3, 10, "u");
+    crosswordProvider.current?.setGuess(3, 2, "Q");
+    crosswordProvider.current?.setGuess(3, 3, "U");
+    crosswordProvider.current?.setGuess(3, 4, "A");
+    crosswordProvider.current?.setGuess(3, 5, "N");
+    crosswordProvider.current?.setGuess(3, 6, "L");
+    crosswordProvider.current?.setGuess(3, 7, "Y");
+  
+  
   }, []);
 
   const fillAnswer5 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(4, 2, "T");
-    crosswordProvider.current?.setGuess(4, 3, "Ấ");
-    crosswordProvider.current?.setGuess(4, 4, "T");
-    crosswordProvider.current?.setGuess(4, 5, "N");
-    crosswordProvider.current?.setGuess(4, 6, "I");
-    crosswordProvider.current?.setGuess(4, 7, "Ê");
-    crosswordProvider.current?.setGuess(4, 8, "N");
+    crosswordProvider.current?.setGuess(4, 3, "B");
+    crosswordProvider.current?.setGuess(4, 4, "A");
+    crosswordProvider.current?.setGuess(4, 5, "O");
+    crosswordProvider.current?.setGuess(4, 6, "V");
+    crosswordProvider.current?.setGuess(4, 7, "E");
+    crosswordProvider.current?.setGuess(4, 8, "T");
+    crosswordProvider.current?.setGuess(4, 9, "A");
+    crosswordProvider.current?.setGuess(4, 10, "I");
+    crosswordProvider.current?.setGuess(4, 11, "S");
+    crosswordProvider.current?.setGuess(4, 12, "A");
+    crosswordProvider.current?.setGuess(4, 13, "N");
+    
   }, []);
 
   const fillAnswer6 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(5, 0, "N");
-    crosswordProvider.current?.setGuess(5, 1, "G");
-    crosswordProvider.current?.setGuess(5, 2, "Ã");
-    crosswordProvider.current?.setGuess(5, 3, "B");
-    crosswordProvider.current?.setGuess(5, 4, "A");
-    crosswordProvider.current?.setGuess(5, 5, "S");
-    crosswordProvider.current?.setGuess(5, 6, "Ô");
+    crosswordProvider.current?.setGuess(5, 5, "C");
+    crosswordProvider.current?.setGuess(5, 6, "O");
     crosswordProvider.current?.setGuess(5, 7, "N");
-    crosswordProvider.current?.setGuess(5, 8, "G");
+    crosswordProvider.current?.setGuess(5, 8, "S");
+    crosswordProvider.current?.setGuess(5, 9, "O");
+   
   }, []);
   const fillAnswer7 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(6, 4, "C");
-    crosswordProvider.current?.setGuess(6, 5, "Â");
-    crosswordProvider.current?.setGuess(6, 6, "U");
-    crosswordProvider.current?.setGuess(6, 7, "Đ");
-    crosswordProvider.current?.setGuess(6, 8, "Ố");
-    crosswordProvider.current?.setGuess(6, 9, "I");
+    crosswordProvider.current?.setGuess(6, 2, "H");
+    crosswordProvider.current?.setGuess(6, 3, "A");
+    crosswordProvider.current?.setGuess(6, 4, "T");
+    crosswordProvider.current?.setGuess(6, 5, "X");
+    crosswordProvider.current?.setGuess(6, 6, "A");
+    crosswordProvider.current?.setGuess(6, 7, "M");
+  
   }, []);
 
   const fillAnswer8 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(7, 6, "H");
-    crosswordProvider.current?.setGuess(7, 7, "Á");
-    crosswordProvider.current?.setGuess(7, 8, "T");
-    crosswordProvider.current?.setGuess(7, 9, "X");
-    crosswordProvider.current?.setGuess(7, 10, "O");
-    crosswordProvider.current?.setGuess(7, 11, "A");
-    crosswordProvider.current?.setGuess(7, 12, "N");
+    crosswordProvider.current?.setGuess(7, 6, "K");
+    crosswordProvider.current?.setGuess(7, 7, "I");
+    crosswordProvider.current?.setGuess(7, 8, "E");
+    crosswordProvider.current?.setGuess(7, 9, "M");
+    crosswordProvider.current?.setGuess(7, 10, "S");
+    crosswordProvider.current?.setGuess(7, 11, "O");
+    crosswordProvider.current?.setGuess(7, 12, "A");
+    crosswordProvider.current?.setGuess(7, 13, "T");
+   
+   
+   
   }, []);
 
   const fillAnswer9 = useCallback<React.MouseEventHandler>((event) => {
     crosswordProvider.current?.setGuess(8, 5, "B");
-    crosswordProvider.current?.setGuess(8, 6, "Á");
+    crosswordProvider.current?.setGuess(8, 6, "A");
     crosswordProvider.current?.setGuess(8, 7, "N");
     crosswordProvider.current?.setGuess(8, 8, "H");
     crosswordProvider.current?.setGuess(8, 9, "C");
     crosswordProvider.current?.setGuess(8, 10, "H");
-    crosswordProvider.current?.setGuess(8, 11, "Ư");
+    crosswordProvider.current?.setGuess(8, 11, "U");
     crosswordProvider.current?.setGuess(8, 12, "N");
-    crosswordProvider.current?.setGuess(8, 13, "N");
+    crosswordProvider.current?.setGuess(8, 13, "G");
+    
   }, []);
 
   const fillAnswer10 = useCallback<React.MouseEventHandler>((event) => {
-    crosswordProvider.current?.setGuess(0, 7, "N");
-    crosswordProvider.current?.setGuess(1, 7, "G");
+    crosswordProvider.current?.setGuess(9, 0, "C");
+    crosswordProvider.current?.setGuess(9, 1, "H");
+    crosswordProvider.current?.setGuess(9, 2, "U");
+    crosswordProvider.current?.setGuess(9, 3, "Y");
+    crosswordProvider.current?.setGuess(9, 4, "E");
+    crosswordProvider.current?.setGuess(9, 5, "N");
+    crosswordProvider.current?.setGuess(9, 6, "K");
+    crosswordProvider.current?.setGuess(9, 7, "H");
+    crosswordProvider.current?.setGuess(9, 8, "O");
+    crosswordProvider.current?.setGuess(9, 9, "A");
+    crosswordProvider.current?.setGuess(9, 10, "N");
+  }, []);
+  const fillAnswer11 = useCallback<React.MouseEventHandler>((event) => {
+    crosswordProvider.current?.setGuess(0, 7, "C");
+    crosswordProvider.current?.setGuess(1, 7, "H");
     crosswordProvider.current?.setGuess(2, 7, "U");
     crosswordProvider.current?.setGuess(3, 7, "Y");
-    crosswordProvider.current?.setGuess(4, 7, "Ê");
+    crosswordProvider.current?.setGuess(4, 7, "E");
     crosswordProvider.current?.setGuess(5, 7, "N");
-    crosswordProvider.current?.setGuess(6, 7, "Đ");
-    crosswordProvider.current?.setGuess(7, 7, "Á");
+    crosswordProvider.current?.setGuess(6, 7, "M");
+    crosswordProvider.current?.setGuess(7, 7, "I");
     crosswordProvider.current?.setGuess(8, 7, "N");
+    crosswordProvider.current?.setGuess(9, 7, "H");
+  
   }, []);
-
   const fillAllAnswersProvider = useCallback<React.MouseEventHandler>(
     (event) => {
       crosswordProvider.current?.fillAllAnswers();
     },
-    [],
+    []
   );
-
+  type ToggleTextButtonProps = {
+    children: ReactNode;
+  };
+  
+  // The ToggleTextButton component with TypeScript
+  const ToggleTextButton: React.FC<ToggleTextButtonProps> = ({ children }) => {
+    const [isVisible, setIsVisible] = useState(false);
+  
+    const toggleVisibility = () => {
+      setIsVisible(!isVisible);
+    };
+  
+    return (
+      <div>
+        <button onClick={toggleVisibility}>
+          {isVisible ? 'Ẩn Câu Hỏi' : 'Hiện Câu Hỏi'}
+        </button>
+        {isVisible && <div>{children}</div>}
+      </div>
+    );
+  };
   const resetProvider = useCallback<React.MouseEventHandler>((event) => {
     crosswordProvider.current?.reset();
   }, []);
@@ -393,7 +456,7 @@ function App() {
     (event) => {
       setMessagesProvider([]);
     },
-    [],
+    []
   );
 
   const addMessageProvider = useCallback((message: string) => {
@@ -415,7 +478,7 @@ function App() {
     (direction, number, answer) => {
       addMessageProvider(`onCorrect: "${direction}", "${number}", "${answer}"`);
     },
-    [addMessageProvider],
+    [addMessageProvider]
   );
 
   // onLoadedCorrect is called with an array of the already-correct answers,
@@ -429,12 +492,12 @@ function App() {
         `onLoadedCorrect:\n${answers
           .map(
             ([direction, number, answer]) =>
-              `    - "${direction}", "${number}", "${answer}"`,
+              `    - "${direction}", "${number}", "${answer}"`
           )
-          .join("\n")}`,
+          .join("\n")}`
       );
     },
-    [addMessageProvider],
+    [addMessageProvider]
   );
 
   // onCrosswordCorrect is called with a truthy/falsy value.
@@ -444,7 +507,7 @@ function App() {
     (isCorrect) => {
       addMessageProvider(`onCrosswordCorrect: ${JSON.stringify(isCorrect)}`);
     },
-    [addMessageProvider],
+    [addMessageProvider]
   );
 
   // onCellChange is called with the row, column, and character.
@@ -454,18 +517,16 @@ function App() {
     (row, col, char) => {
       addMessageProvider(`onCellChange: "${row}", "${col}", "${char}"`);
     },
-    [addMessageProvider],
+    [addMessageProvider]
   );
-  // BƯỚC 3: ĐIỀN CÂU LỆNH VỪA NHẬP Ở BƯỚC 2 VÀO:
-  // <Command onClick={CÂU LỆNH }>Hiện đáp án </Command>
-  // VÍ DỤ <Command onClick={fillAnswer10}>Hiện đáp án 10</Command>
+
   return (
     <div className="page">
       <Page>
         <Header className="headerPage">PHẦN THI VƯỢT QUA THỬ THÁCH</Header>
 
         <CrosswordMessageBlock>
-          <Commands>
+        <Commands>
             <Command onClick={fillAnswer1}>Hiện đáp án 1</Command>
             <Command onClick={fillAnswer2}>Hiện đáp án 2</Command>
             <Command onClick={fillAnswer3}>Hiện đáp án 3</Command>
@@ -476,12 +537,11 @@ function App() {
             <Command onClick={fillAnswer8}>Hiện đáp án 8</Command>
             <Command onClick={fillAnswer9}>Hiện đáp án 9</Command>
             <Command onClick={fillAnswer10}>Hiện đáp án 10</Command>
-            <Command onClick={fillAllAnswersProvider}>
-              Điền toàn bộ câu trả lời
-            </Command>
+            <Command onClick={fillAnswer11}>Đáp án ô chữ bí ẩn</Command>
+            
+
             <Command onClick={resetProvider}>Reset</Command>
           </Commands>
-
           <CrosswordProviderWrapper>
             <CrosswordProvider
               ref={crosswordProvider}
@@ -504,6 +564,25 @@ function App() {
               <CrosswordGrid />
             </CrosswordProvider>
           </CrosswordProviderWrapper>
+          <div className="question">
+            <h1>Câu Hỏi</h1>
+            <ol>
+              <li><ToggleTextButton>Không làm việc riêng khi có Khách hàng là thể hiện tiêu chí nào trong bộ tiêu chuẩn CLDV?</ToggleTextButton></li> 
+              <li><ToggleTextButton>Tên sản phẩm tiền gửi tiết kiệm trong đó khách hàng có cam kết về thời gian gửi tiền?</ToggleTextButton></li>
+              <li><ToggleTextButton>Đây là tên Năng lực cốt lõi thứ 3?</ToggleTextButton></li>
+              <li><ToggleTextButton>Điền từ vào dấu … để hoàn thiện tên một trong 05 năng lực lãnh đạo: "… sự thay đổi"?</ToggleTextButton></li>
+              <li><ToggleTextButton>Tên Điều 26 trong NQLĐ trong đó có quy định: Người lao động có trách nhiệm bảo vệ phương tiện làm việc và các tài sản khác của NCB trong suốt quá trình làm việc tại NCB?</ToggleTextButton></li>
+              <li><ToggleTextButton>Theo cách gọi của người Việt Nam, nhà hát Opera còn có tên gọi khác là gì?</ToggleTextButton></li>
+              <li><ToggleTextButton>Tên loại hình nghệ thuật khi nhắc đến nghệ nhân Hà Thị Cầu?</ToggleTextButton></li>
+              <li><ToggleTextButton>Một khâu quan trọng giúp hạn chế sai lỗi phát sinh trong giao dịch?</ToggleTextButton></li>
+              <li><ToggleTextButton>Cái gì trong trắng ngoài xanh, trồng đậu trồng hành rồi thả heo vào?</ToggleTextButton></li>
+              <li><ToggleTextButton>Một hình thức thanh toán không dùng tiền mặt?</ToggleTextButton></li>
+              
+             
+            </ol>
+            <h3> </h3> 
+            <h3></h3>
+          </div>
         </CrosswordMessageBlock>
       </Page>
     </div>
